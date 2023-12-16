2b32007443be83791671f608dc0b52571b128e7d
DC20-dev <dany2798@gmail.com>
2023-12-16 23:24:54 +0100

    * changed CMakeLists to have different name for libraries. * modified export.h to be able to switch to dllimport with CHIP8_IMPORT defined

--- include/export.h
+++ include/export.h
@@ -1,7 +1,11 @@
 #pragma once
 
 #ifdef _WIN32
+#ifdef CHIP8_IMPORT
+#define CHIP8_API __declspec(dllimport)
+#else
 #define CHIP8_API __declspec(dllexport)
+#endif // CHIP8_IMPORT
 #else
 #define CHIP8_API
 #endif // _WIN32
